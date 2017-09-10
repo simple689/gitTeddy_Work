78f8df6d9b93711e6638da313b13f1554991b287
ZhaoHuan <teddy689@qq.com>
2017-09-10 14:21:41 +0800

    1

---
+++ Teddy_Work_ServerCenter/TeddyNetCoreProject/TeddyNetCore_EngineFrame/src/Main/EngineFrame.cs
@@ -0,0 +1,13 @@
+﻿using TeddyNetCore_Engine;
+
+namespace TeddyNetCore_EngineFrame {
+    class EngineFrame {
+        static void Main(string[] args) {
+            EngineManager engineManager = new EngineManager();
+            engineManager.init(args);
+            engineManager.start();
+            engineManager.update();
+            engineManager.stop();
+        }
+    }
+}
---
+++ Teddy_Work_ServerManager/TeddyNetCoreProject/TeddyNetCore_ServerManager_Web/Controllers/ServerCenterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeddyNetCore_ServerManager_Web {
+    public class ServerCenterController : Controller {
+        public IActionResult Index(string a) {
+            return View();
+        }
+
+        public IActionResult Error() {
+            return View();
+        }
+
+        public string test() {
+            return "test";
+        }
+    }
+}
